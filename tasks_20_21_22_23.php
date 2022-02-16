e56b2d48f0b6c35815514009d696c794e87dcce1
Константин <info@yroom.ru>
2022-02-16 16:56:27 +0300

    Update tasks_20_21_22_23.php

--- tasks_20_21_22_23.php
+++ tasks_20_21_22_23.php
@@ -5,6 +5,6 @@ function cube($num){
 }
 echo cube(10), '<br>';
 echo $_GET["arr1"] + $_GET["arr2"], '<br>';
-foreach ($_GET as $var => $val){
-    echo $var." = ".$val."<br>";
+foreach ($_GET as $key => $val){
+    echo $key." = ".$val."<br>";
 }
